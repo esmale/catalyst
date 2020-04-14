39ca1fb82b82bc87b4014a21f5178774e1bfc2c9
David Hill <dhill@muvehealth.com>
2020-04-14 09:59:37 -0500

    paring down how much the template attempts to do

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def bootstrap_class_for(flash_type)
+  def tailwind_classes_for(flash_type)
     {
-      success: "alert-success",
-      error: "alert-danger",
-      alert: "alert-warning",
-      notice: "alert-info"
+      success: "bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative",
+      error: "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative",
+      alert: "bg-orange-100 border border-orange-400 text-orange-700 px-4 py-3 rounded relative",
+      notice: "bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 rounded relative"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("local-time").start()
+require("stylesheets/application.scss")
 
 window.Rails = Rails
-
-import 'bootstrap'
-import 'data-confirm-modal'
-
-$(document).on("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-})
