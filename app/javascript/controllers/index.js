b1f4a75ce04c051a1d93cc7f1451ea5ad786aed7
David Hill <david.esmale@gmail.com>
2020-08-31 10:33:10 -0500

    setup inital integration with tailwindcss-stimulus-components

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,18 @@
+// Load all the controllers within this directory and all subdirectories.
+// Controller files must be named *_controller.js.
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
+
+// Import and register all TailwindCSS Components
+import { Alert, Dropdown, Modal, Tabs, Popover, Toggle } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
